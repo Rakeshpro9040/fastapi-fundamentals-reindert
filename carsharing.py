01c75589f82c4a58979a26256e7ec139c7a2007c
Rakesh Az <rakesh.panigrahi.git1@gmail.com>
2023-09-19 16:20:29 +0530

    List all car ids

--- carsharing.py
+++ carsharing.py
@@ -3,6 +3,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
+
+
+@app.get("/api/cars")
+def get_cars():
+    ids = [x["id"] for x in db]
+    """Return all cars"""
+    return ids
+
 
 @app.get("/")
 def welcome(name):
